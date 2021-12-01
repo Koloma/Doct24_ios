47f43fc3db51e10951fee56d32b18d83c5f432f2
Alexandr Kolomenskiy <koloma2@mail.ru>
2021-12-01 23:01:35 +0700

    change Const colors

--- doct24_ios/Core/Constants.swift
+++ doct24_ios/Core/Constants.swift
@@ -5,24 +5,24 @@
 //  Created by Alexander Kolomenskiy on 01.12.2021.
 //
 
-import SwiftUI
+import UIKit
 
 struct Const {
 
-    private struct Colors {
+    public struct Colors {
 
-        static let BasicDarck = UIColor(red: 0.024, green: 0.165, blue: 0.247, alpha: 1).cgColor
-        static let MainGreen  = UIColor(red: 0.098, green: 0.745, blue: 0.435, alpha: 1).cgColor
-        static let Main       = UIColor(red: 0.024, green: 0.655, blue: 0.702, alpha: 1).cgColor
-        static let BasicGrey  = UIColor(red: 0.636, green: 0.64, blue: 0.658, alpha: 1).cgColor
-        static let BasicLightGrey  = UIColor(red: 0.958, green: 0.958, blue: 0.958, alpha: 1).cgColor
-        static let BasicWhite  = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
-        static let SecondaryBlue  = UIColor(red: 0.878, green: 0.937, blue: 0.965, alpha: 1).cgColor
-        static let SecondaryPink  = UIColor(red: 0.988, green: 0.969, blue: 1, alpha: 1).cgColor
-        static let SecondaryYellow  = UIColor(red: 0.988, green: 0.718, blue: 0.106, alpha: 1).cgColor
+        static let BasicDarck = UIColor(red: 0.024, green: 0.165, blue: 0.247, alpha: 1)
+        static let MainGreen  = UIColor(red: 0.098, green: 0.745, blue: 0.435, alpha: 1)
+        static let Main       = UIColor(red: 0.024, green: 0.655, blue: 0.702, alpha: 1)
+        static let BasicGrey  = UIColor(red: 0.636, green: 0.64, blue: 0.658, alpha: 1)
+        static let BasicLightGrey  = UIColor(red: 0.958, green: 0.958, blue: 0.958, alpha: 1)
+        static let BasicWhite  = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        static let SecondaryBlue  = UIColor(red: 0.878, green: 0.937, blue: 0.965, alpha: 1)
+        static let SecondaryPink  = UIColor(red: 0.988, green: 0.969, blue: 1, alpha: 1)
+        static let SecondaryYellow  = UIColor(red: 0.988, green: 0.718, blue: 0.106, alpha: 1)
         //F0FAFF
-        static let SecondaryNoName  = UIColor(red: 0.942, green: 0.981, blue: 1, alpha: 1).cgColor
-        static let SecondaryRed  = UIColor(red: 1, green: 0.212, blue: 0.212, alpha: 1).cgColor
+        static let SecondaryNoName  = UIColor(red: 0.942, green: 0.981, blue: 1, alpha: 1)
+        static let SecondaryRed  = UIColor(red: 1, green: 0.212, blue: 0.212, alpha: 1)
 
     }
 }
