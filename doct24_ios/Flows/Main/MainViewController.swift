d57e805aa0a860c9a18765e5a02a256e497b9823
Alexandr Kolomenskiy <koloma2@mail.ru>
2021-12-01 20:41:28 +0700

    Add Fonts and Colors

--- doct24_ios/Flows/Main/MainViewController.swift
+++ doct24_ios/Flows/Main/MainViewController.swift
@@ -11,6 +11,7 @@ class MainViewController: UIViewController {
 
     @IBOutlet weak var headerView: HeaderView!
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var midleView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,10 +20,31 @@ class MainViewController: UIViewController {
     }
 
     func setupView() {
-
-
+        
+        addTextLable()
 
     }
 
+
+    func addTextLable() {
+
+        let lable = UILabel()
+        lable.frame = CGRect(x: 0, y: 0, width: 202, height: 28)
+        lable.backgroundColor = .white
+
+        lable.textColor = UIColor(red: 0.024, green: 0.655, blue: 0.702, alpha: 1)
+        lable.font = UIFont(name: "Rubik-Medium", size: 28)
+
+        lable.text = "Лучшие врачи"
+
+        let parent = midleView!
+        parent.addSubview(lable)
+        lable.translatesAutoresizingMaskIntoConstraints = false
+        lable.widthAnchor.constraint(equalToConstant: 202).isActive = true
+        lable.heightAnchor.constraint(equalToConstant: 28).isActive = true
+        lable.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: 86).isActive = true
+        lable.topAnchor.constraint(equalTo: parent.topAnchor, constant: 277).isActive = true
+
+    }
 }
 
