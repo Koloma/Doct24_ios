ecd1018b7d2308c9e2fcd297b9e65b4de37490ad
Alexandr Kolomewskiy <koloma2@mail.ru>
2021-12-05 11:53:31 +0700

    correct Constraints for SearchBar

--- doct24_ios/Flows/Doctors/SpecializationViewController.swift
+++ doct24_ios/Flows/Doctors/SpecializationViewController.swift
@@ -15,4 +15,12 @@ class SpecializationViewController: UIViewController {
        print("Specialization")
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
 }
